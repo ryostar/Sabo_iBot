65f90590e3e0a47265f8cbdd7af9d90559b06791
ryostar <82922436+ryostar@users.noreply.github.com>
2021-06-13 01:23:17 +0700

    Update admins.py

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -37,10 +37,10 @@ async def update_admin(client, message: Message):
             for member in await message.chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("❇️ Admin cache refreshed!")
+    await message.reply_text("❇️ Đã làm mới bộ nhớ cache của quản trị viên!")
 
 
-@Client.on_message(command("pause") & other_filters)
+@Client.on_message(command("tamdung") & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
@@ -48,13 +48,13 @@ async def pause(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "paused"
     ):
-        await message.reply_text("❗ Nothing is playing!")
+        await message.reply_text("❗ Không có gì để dừng!")
     else:
         callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("▶️ Paused!")
+        await message.reply_text("▶️ Tạm dừng phát nhạc!")
 
 
-@Client.on_message(command("resume") & other_filters)
+@Client.on_message(command("tieptuc") & other_filters)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
@@ -62,19 +62,19 @@ async def resume(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "playing"
     ):
-        await message.reply_text("❗ Nothing is paused!")
+        await message.reply_text("❗ Không có nhạc đang tạm dừng!")
     else:
         callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("⏸ Resumed!")
+        await message.reply_text("⏸ Đã tiếp tục phát nhạc!")
 
 
-@Client.on_message(command("end") & other_filters)
+@Client.on_message(command("off") & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is streaming!")
+        await message.reply_text("❗ Không có trên cuộc gọi thoại!")
     else:
         try:
             callsmusic.queues.clear(chat_id)
@@ -82,17 +82,17 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("❌ Stopped streaming!")
+        await message.reply_text("❌ Đã ngừng phát nhạc!")
 
 
-@Client.on_message(command("skip") & other_filters)
+@Client.on_message(command("boqua") & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is playing to skip!")
+        await message.reply_text("❗ Không có bài hát nào để bỏ qua!")
     else:
         callsmusic.queues.task_done(chat_id)
 
@@ -108,7 +108,7 @@ async def skip(_, message: Message):
         skip = qeue.pop(0)
     if not qeue:
         return
-    await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+    await message.reply_text(f"- Bỏ qua **{skip[0]}**\n- Bắt đầu phát **{qeue[0][0]}**")
 
 
 @Client.on_message(filters.command("admincache"))
@@ -121,4 +121,4 @@ async def admincache(client, message: Message):
             for member in await message.chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("❇️ Admin cache refreshed!")
+    await message.reply_text("❇️ Đã làm mới bộ nhớ cache của quản trị viên!")
