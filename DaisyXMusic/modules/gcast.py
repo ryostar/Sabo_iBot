323b09bea923c5128a5a5df42afb05fa68177253
ryostar <82922436+ryostar@users.noreply.github.com>
2021-06-13 02:31:38 +0700

    Update gcast.py

--- DaisyXMusic/modules/gcast.py
+++ DaisyXMusic/modules/gcast.py
@@ -20,23 +20,23 @@ from pyrogram.errors import UserAlreadyParticipant
 import asyncio
 from DaisyXMusic.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
+@Client.on_message(filters.command(["ib"]))
 async def bye(client, message):
     sent=0
     failed=0
     if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+        lol = await message.reply("Khởi động IB")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await lol.edit("Trả lời bất kỳ tin nhắn văn bản nào tới IB thưa ông")
             return
         msg = message.reply_to_message.text
         async for dialog in client.iter_dialogs():
             try:
                 await client.send_message(dialog.chat.id, msg)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await lol.edit(f"IB.. Đang gửi: {sent} cuộc trò chuyện. Thất bại: {failed} cuộc trò chuyện.")
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await lol.edit(f"IB.. Đang gửi: {sent} cuộc trò chuyện. Thất bại: {failed} cuộc trò chuyện.")
             await asyncio.sleep(3)
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+        await message.reply_text(f"Đã gửi tin nhắn tới {sent} cuộc trò chuyện. Thất bại {failed} cuộc trò chuyện.")
